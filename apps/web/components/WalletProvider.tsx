f0d26fb87fdab83f7c09a476d4df0fdd5f6e595c
Koss <danknobs@gmail.com>
2025-11-22 01:15:59 -0300

    Update WalletProvider.tsx

--- apps/web/components/WalletProvider.tsx
+++ apps/web/components/WalletProvider.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import * as React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { wagmiConfig } from '../lib/walletConfig';
 
@@ -8,7 +9,13 @@ type WalletProviderProps = {
   children: React.ReactNode;
 };
 
+const queryClient = new QueryClient();
+
 export function WalletProvider({ children }: WalletProviderProps) {
-  return <WagmiProvider config={wagmiConfig}>{children}</WagmiProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <WagmiProvider config={wagmiConfig}>{children}</WagmiProvider>
+    </QueryClientProvider>
+  );
 }
 
