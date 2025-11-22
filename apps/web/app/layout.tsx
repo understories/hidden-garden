2d392a81e7ade0bc759cfb3b44ff1cf162a740b2
vrnvrn <vrneth@proton.me>
2025-11-21 22:22:05 -0300

    Create layout.tsx

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Hidden Garden',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
+
