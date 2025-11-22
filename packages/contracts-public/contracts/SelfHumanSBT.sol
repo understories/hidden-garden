9cc4a80bcea2ce3cbb3affe9d92526355970bc21
vrnvrn <vrneth@proton.me>
2025-11-21 23:37:42 -0300

    add comments

--- packages/contracts-public/contracts/SelfHumanSBT.sol
+++ packages/contracts-public/contracts/SelfHumanSBT.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.8.20;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 
+/// @title SelfHumanSBT
+/// @notice Stub contract for integration with Self. Issues non-transferable SBTs to verified humans.
 contract SelfHumanSBT is ERC721 {
     event HumanVerified(address indexed user, uint256 tokenId);
 
     constructor() ERC721('SelfHumanSBT', 'SHSBT') {}
 
+    /// @notice Verifies and mints an SBT for the caller
+    /// @dev Currently mints unconditionally. Will later validate `proof` parameter before minting.
+    /// @param proof Proof data for human verification (currently unused, will be validated in future)
     function verifyAndMint(bytes calldata proof) external {
         address user = msg.sender;
         uint256 tokenId = uint256(uint160(user));
