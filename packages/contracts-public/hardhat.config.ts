cfdf0e108a0f31a19266e11eefaa9574faff804e
vrnvrn <vrneth@proton.me>
2025-11-21 23:20:08 -0300

    downgrade hardhat version

--- packages/contracts-public/hardhat.config.ts
+++ packages/contracts-public/hardhat.config.ts
@@ -2,16 +2,7 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: '0.8.20',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
-  defaultNetwork: 'hardhat',
+  solidity: '0.8.20',
   networks: {
     hardhat: {},
     celoTestnet: {
