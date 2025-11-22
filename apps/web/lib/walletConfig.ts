dae03af2781a22fa9638d06f241f244fa607370b
Koss <danknobs@gmail.com>
2025-11-22 01:09:03 -0300

    Update walletConfig.ts

--- apps/web/lib/walletConfig.ts
+++ apps/web/lib/walletConfig.ts
@@ -1,9 +1,11 @@
 import { http, createConfig } from 'wagmi';
 import { sepolia } from 'wagmi/chains';
+import { injected } from 'wagmi/connectors';
 
 // In the future we may switch to Celo or other chains.
 export const wagmiConfig = createConfig({
   chains: [sepolia],
+  connectors: [injected()],
   transports: {
     [sepolia.id]: http(
       process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL ??
