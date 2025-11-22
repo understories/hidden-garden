cdf43f29a3a4f01dd0b97d180be0e6f877178bd4
vrnvrn <vrneth@proton.me>
2025-11-21 22:51:55 -0300

    Create Greeter.sol

---
+++ packages/contracts-public/contracts/Greeter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Greeter {
+    string private greeting;
+
+    constructor(string memory _greeting) {
+        greeting = _greeting;
+    }
+
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+}
+
