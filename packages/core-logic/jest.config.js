808ceedb522ff8735c10f0ab2b12c98dbaae1971
vrnvrn <vrneth@proton.me>
2025-11-22 21:05:12 -0300

    Update jest.config.js

--- packages/core-logic/jest.config.js
+++ packages/core-logic/jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/tests', '<rootDir>/../../tests'],
+  roots: ['<rootDir>/tests'],
   testMatch: ['**/*.test.ts', '**/tests/**/*.test.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
